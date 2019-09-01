27149382455a3e83176d198fb3416d8997037fac
ekistece <mr.tamuraa@gmail.com>
2019-09-01 22:38:43 +0200

    some fixes

--- liveWallpaper.sh
+++ liveWallpaper.sh
@@ -2,10 +2,13 @@
 
 CONFIG="$HOME/.wallpaper"
 
+cd "$( cd "$(dirname "$0")" ; pwd -P )"
+
 if [ "$1" ]; then
     echo "$1" > $HOME/.wallpaper;
 fi
 WP_PATH=$(cat $CONFIG)
+
 
 mpv "$WP_PATH" --no-audio --loop --no-input-default-bindings --no-osc --title="MPV Wallpaper" --hwdec=vaapi --vo=opengl > /dev/null 2>&1 &
 devilspie mpv.ds
